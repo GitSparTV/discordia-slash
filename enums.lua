f053473e1dbcd65fb4bc0c1c3b1e57b420d86087
GitSparTV <developspartv@gmail.com>
2021-04-02 17:30:41 +0300

    New enumerations, remove deprecated enums

--- enums.lua
+++ enums.lua
@@ -1,31 +1,27 @@
 local enum = require('discordia').enums.enum
 
-local ApplicationCommandOptionType = enum({
-	subCommand = 1,
-	subCommandGroup = 2,
-	string = 3,
-	integer = 4,
-	boolean = 5,
-	user = 6,
-	channel = 7,
-	role = 8
-})
-
-local InteractionType = enum({
-	ping = 1,
-	applicationCommand = 2
-})
-
-local InteractionResponseType = enum({
-	pong = 1,
-	acknowledge = 2,
-	channelMessage = 3,
-	channelMessageWithSource = 4,
-	acknowledgeWithSource = 5
-})
-
 return {
-	optionType = ApplicationCommandOptionType,
-	interactionType = InteractionType,
-	interactionResponseType = InteractionResponseType
+	optionType = enum({
+		subCommand = 1,
+		subCommandGroup = 2,
+		string = 3,
+		integer = 4,
+		boolean = 5,
+		user = 6,
+		channel = 7,
+		role = 8
+	}),
+	interactionType = enum({
+		ping = 1,
+		applicationCommand = 2
+	}),
+	interactionResponseType = enum({
+		pong = 1,
+		channelMessageWithSource = 4,
+		deferredChannelMessageWithSource = 5
+	}),
+	applicationCommandPermissionType = enum({
+		role = 1,
+		user = 2,
+	})
 }
