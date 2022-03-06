d6a7c999c1610b41101815b40129eebdf8c04545
GitSparTV <developspartv@gmail.com>
2022-03-07 00:47:47 +0300

    Remove unused file

--- endpoints.lua
+++
@@ -1,8 +0,0 @@
-return {
-	APPLICATION_COMMAND                       = "/applications/%s/commands/%s",
-	APPLICATION_COMMANDS                      = "/applications/%s/commands",
-	APPLICATION_GUILD_COMMAND                 = "/applications/%s/guilds/%s/commands/%s",
-	APPLICATION_GUILD_COMMANDS                = "/applications/%s/guilds/%s/commands",
-	APPLICATION_GUILD_COMMANDS_PERMISSIONS    = "/applications/%s/guilds/%s/commands/permissions",
-	APPLICATION_GUILD_COMMAND_PERMISSIONS     = "/applications/%s/guilds/%s/commands/%s/permissions"
-}
